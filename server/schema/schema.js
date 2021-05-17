85bd76bf889c623c1f14de52464450a159d1090f
redidizzy <z.rediane24@gmail.com>
2021-05-17 10:40:00 +0100

    Add schema

---
+++ server/schema/schema.js
@@ -0,0 +1,82 @@
+const graphql = require('graphql')
+
+const { 
+  GraphQLObjectType, 
+  GraphQLString, 
+  GraphQLSchema,
+  GraphQLID
+ } = graphql
+
+const books = [
+  { name: 'Name of the wind', genre: 'Fantasy', id: '1' },
+  { name: 'The Final Empire', genre: 'Fantasy', id: '2' },
+  { name: 'The long earth', genre: 'Sci-Fi', id: '3' }
+] 
+
+const authors = [
+  { name: 'Rediane Zemmouri', age: 24, id: '1' },
+  { name: 'Amine Azizi', age:28, id: '2' },
+  { name: 'Redi dizzy', age: 40, id: '3' }
+]
+
+const BookType = new GraphQLObjectType({
+  name: 'Book',
+  fields: () => ({
+    id: {
+      type: GraphQLID
+    },
+    name: {
+      type: GraphQLString
+    },
+    genre: {
+      type: GraphQLString
+    }
+  })
+})
+
+const AuthorType = new GraphQLObjectType({
+  name: 'Author',
+  fields: () => ({
+    id: {
+      type: GraphQLID
+    },
+    name: {
+      type: GraphQLString
+    },
+    age: {
+      type: GraphQLString
+    }
+  })
+})
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    book: {
+      type: BookType,
+      args: { 
+        id: {
+          type: GraphQLID
+        }
+      },
+      resolve(parent, args){
+        return books.find( book => book.id === args.id )
+      }
+    },
+    author: {
+      type: AuthorType,
+      args: {
+        id: {
+          type: GraphQLID
+        }
+      },
+      resolve(parent, args){
+        return authors.find(author => author.id === args.id)
+      }
+    }
+  }
+})
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
+})
