794ba6c1c950d0a51a34c23549a010716c62b315
redidizzy <z.rediane24@gmail.com>
2021-05-17 11:08:51 +0100

    Add final root queries

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -86,6 +86,18 @@ const RootQuery = new GraphQLObjectType({
       resolve(parent, args){
         return authors.find(author => author.id === args.id)
       }
+    },
+    books: {
+      type: new GraphQLList(BookType),
+      resolve(){
+        return books
+      }
+    },
+    authors: {
+      type: new GraphQLList(AuthorType),
+      resolve(){
+        return authors
+      }
     }
   }
 })
